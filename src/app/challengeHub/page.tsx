8a31f6f1b7bcde42925abd893580e91735bf253a
DarrenT <darren243@hotmail.com>
2025-10-23 12:40:56 +0800

    modified path for challenge hub

--- src/app/challengeHub/page.tsx
+++ src/app/challengeHub/page.tsx
@@ -1,25 +1,32 @@
-import Breadcrumb from '@/common/components/Breadcrumb';
-import Challenges from '../../features/challengeHub/component/ChallengeHub';
-import Header from '@/common/components/Header';
-import styles from './ChallengePage.module.css'
+"use client";
+import { useEffect, useState } from "react";
+
+import Breadcrumb from "@/common/components/Breadcrumb";
+import Challenges from "../../features/challengeHub/component/ChallengeHub";
+import Header from "@/common/components/Header";
+import styles from "./ChallengePage.module.css";
 
 const CHALLENGES_BREADCRUMBS = [
-   { name: "Home", href: "/" },
-   { name: "Chill Challenge Hub", href: "/challengeHub" },
- ]
+  { name: "Home", href: "/" },
+  { name: "Chill Challenge Hub", href: "/challengeHub" },
+];
 
-export default async function ChallengePage() {
-   const response = await fetch('http://localhost:3000/api/challengeHub', { cache: 'no-store' });
-   const data = await response.json();
-   return (
-      <div>
-         <Breadcrumb links={CHALLENGES_BREADCRUMBS}/>
-         <Header 
-            headerText='Chill Challenge Hub' 
-            captionText='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' 
-            containerClassName={styles.container}
-         />
-         <Challenges challenges={data}/>
-      </div>
-   );
+export default function ChallengePage() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("/api/challengeHub")
+      .then((r) => r.json())
+      .then(setData);
+  }, []);
+  return (
+    <div>
+      <Breadcrumb links={CHALLENGES_BREADCRUMBS} />
+      <Header
+        headerText="Chill Challenge Hub"
+        captionText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        containerClassName={styles.container}
+      />
+      <Challenges challenges={data} />
+    </div>
+  );
 }
